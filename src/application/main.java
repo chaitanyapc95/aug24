e2898d82db09af027480598b83eaba526f065681
1896052 <1896052@E21044.CEGEPGIM.LCL>
2019-08-24 13:24:28 -0400

    initial commit

---
+++ src/application/main.java
@@ -0,0 +1,19 @@
+
+package application;
+
+/**
+ *
+ * @author 1896052
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+    
+}
